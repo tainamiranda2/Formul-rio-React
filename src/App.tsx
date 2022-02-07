53082617eba8b6011b0558508f6a28918ee6eb4d
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-02-07 12:10:59 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import './App.css'
+;
+
+
+const App=() =>{
+  return (
+
+    <>...</>
+
+  )
+}
+
+export default App;
---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import {FormStep1} from './pages/FormStep1'
+
+import {FormStep2} from './pages/FormStep2'
+
+import {FormStep1} from './pages/FormStep3'
+
+export const Router = () =>{
+  <BrowserRouter>
+  <Route path="/" exact component={FormStep1}/>
+  
+<Route path="/step2" exact component={FormStep2}/>
+
+<Route path="/step3" exact component={FormStep3}/>
+  </BrowserRouter>
+}
